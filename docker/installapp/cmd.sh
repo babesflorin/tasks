bbec16abd80ab5b5553348b0f9723de8e932c17e
Florin Babes <holysleeper@users.noreply.github.com>
2020-03-12 14:29:35 +0200

    Update cmd.sh

--- docker/installapp/cmd.sh
+++ docker/installapp/cmd.sh
@@ -2,6 +2,6 @@
 /wait-for-it.sh mysql:3306 
 /wait-for-it.sh php-fpm:9000
 
-php bin/console doctine:database:create --if-not-exists 
+php bin/console doctrine:database:create --if-not-exists 
 
 php bin/console --no-interaction doctrine:migrations:migrate
